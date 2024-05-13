f2cd67f557419fc450866201807b417a00c0b79d
Kunika <kunikabiyani20@gmail.com>
2024-05-13 19:39:06 +0900

    Updating the Search option

--- src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const ExploreTopBooks = () => {
     return (
-        <div className="p-5 mb-4 bg-dark header">
-            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+        <div className='p-5 mb-4 bg-dark header'>
+            <div className='container-fluid py-5 text-white 
+                d-flex justify-content-center align-items-center'>
                 <div>
-                    <h1 className="display-5 fw-bold">Find your next Adevnture</h1>
-                    <p className="col-md-8 fs-4">Where would you like to go next?</p>
-                    <a type="button" href="#" className="btn main-color btn-lg  btn-outline-light text-white">Explore Top Books</a>
+                    <h1 className='display-5 fw-bold'>Find your next adventure</h1>
+                    <p className='col-md-8 fs-4'>Where would you like to go next?</p>
+                    <Link type='button' className='btn main-color btn-lg text-white' to='/search'>
+                        Explore top books</Link>
                 </div>
             </div>
         </div>
